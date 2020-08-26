f3174f49552ae2f10847f102691fbf3022df1842
lalit_kumar <lalitrajput069@gmail.com>
2020-08-26 22:33:04 +0530

    Phone number verification added (rough work now)

--- app/src/main/java/com/example/letschat/MainActivity.java
+++ app/src/main/java/com/example/letschat/MainActivity.java
@@ -1,56 +1,97 @@
 package com.example.letschat;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.LogPrinter;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-EditText phoneNumber, editTextVerification;
-Button button;
-PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    EditText phoneNumber, editTextVerification;
+    Button button;
+    String number;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        phoneNumber=findViewById(R.id.phone);
-        button =findViewById(R.id.button);
+        phoneNumber = findViewById(R.id.phone);
+        button = findViewById(R.id.button);
         editTextVerification = findViewById(R.id.verification);
-        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            public void onClick(View v) {
+                verifyPhoneNumber();
+            }
+        });
+
+            mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted( PhoneAuthCredential phoneAuthCredential) {
+
                 signInWithPhoneAuthCredential(phoneAuthCredential);
+
             }
 
             @Override
-            public void onVerificationFailed(@NonNull FirebaseException e) {
+            public void onVerificationFailed( FirebaseException e) {
 
             }
-        };
 
-           sentVerificationCode();
+                @Override
+                public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                    super.onCodeSent(s, forceResendingToken);
+
+                }
+            };
 
     }
 
-    private void sentVerificationCode() {
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNumber.getText().toString(),
-                60,
-                TimeUnit.SECONDS,
-                this,
-                mCallbacks);
-    }
+private void verifyPhoneNumber(){
+
+    PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNumber.getText().toString(),
+            60, TimeUnit.SECONDS,this,mCallbacks);
+
+}
 
 
     private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete( Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            userLoggedIN();
+                        }
+                    }
+                });
+    }
 
+    private void userLoggedIN() {
+        FirebaseUser user;
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            startActivity(new Intent(getApplicationContext(),MainPage.class));
+        }
     }
 }
